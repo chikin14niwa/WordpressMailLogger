24e6fd3b3afaa3988e2760d9745cd9777e19944f
Hayato Nakamura <hyt.rugby@gmail.com>
2022-10-06 23:44:39 +0900

    feat: とりあえずPluginとして成立するところまで

---
+++ wordpress-mail-logger.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Wordpress Mail Logger
+ * Description: Save file when using wp_mail()
+ * Version: 1.0
+ * License: GPL v3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Author: Hayato Nakamura
+ * Author URI: https://github.com/chikin14niwa
+ *
+ * Wordpress Mail Logger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Wordpress Mail Logger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wordpress Mail Logger. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+ */
+namespace Plugin\WordpressMailLogger;
+
+if (!defined('WPINC')) {
+  die;
+}
